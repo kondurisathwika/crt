bb2e81b7050e86d1d78e8a51957d50e4ed5676d1
kondurisathwika <kondurisathwika@gmail.com>
2024-06-24 23:09:26 +0530

    Array reverse

---
+++ Arrayinputreverse.java
@@ -0,0 +1,14 @@
+class Arrayinputreverse
+{
+public static void main(String[] args)
+{
+int a[]=new int[3];
+a[0]=25;
+a[1]=91;
+a[2]=33;
+for(int i=2;i>=0;i--)
+{
+System.out.println(a[i]);
+}
+}
+}
