1edd90d642f5ccbc160d2a7554b9a033d1f26f83
kondurisathwika <kondurisathwika@gmail.com>
2024-06-24 23:06:09 +0530

    array length

---
+++ Arraylength.java
@@ -0,0 +1,13 @@
+class Arraylength
+{
+public static void main(String[] args)
+{
+int a[]={44,67,31,28,79};
+System.out.println(a.length);
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+//System.out.println(a.length);
+}
+}
