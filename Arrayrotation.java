ba95bd31249f1d46f4acf2593e426092d37fa035
kondurisathwika <kondurisathwika@gmail.com>
2024-07-01 09:05:39 +0530

    Array rotation

---
+++ Arrayrotation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Arrayrotation
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+int k=s.nextInt();
+for(int i=k;i<n;i++)
+{
+System.out.print(a[i]+" ");
+}
+for(int i=0;i<k;i++)
+{
+System.out.print(a[i]+" ");
+}
+}
+}
