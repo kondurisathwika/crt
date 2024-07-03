241cba60779696100ed3b978bf24fd0c9f8a57a0
kondurisathwika <kondurisathwika@gmail.com>
2024-07-03 23:24:55 +0530

    convert string lower to upper,upper to lower

---
+++ Lowerupper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Lowerupper
+{
+public static void main(String args[])
+{
+String str="Python is Simple Language";
+System.out.println(str.toLowerCase());
+System.out.println(str.toUpperCase());
+}
+}
