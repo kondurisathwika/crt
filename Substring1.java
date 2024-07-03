223cb5b374a7e13c4f533a32d40d2b6845402b0c
kondurisathwika <kondurisathwika@gmail.com>
2024-07-03 23:22:56 +0530

    substring

---
+++ Substring1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Substring1
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str="Java is a Simple Language";
+System.out.println(str.substring(6));
+System.out.println(str.substring(3,15));
+}
+}
+
