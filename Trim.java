27d9b587296b20b5fc1573e3277b08707dba589c
kondurisathwika <kondurisathwika@gmail.com>
2024-07-03 23:18:19 +0530

    Trim in string

---
+++ Trim.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Trim
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str="  java is   ";
+System.out.println(str.length());
+String str1=str.trim();
+System.out.println(str1.length());
+System.out.println(str1);
+}
+}
+
