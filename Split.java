fde4ad6cdc1129dce009f791f69a8f9b49dd1135
kondurisathwika <kondurisathwika@gmail.com>
2024-07-03 23:22:05 +0530

    split the string

---
+++ Split.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Split
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str="Java is a Simple Language";
+String res[]=str.split(" ");
+System.out.println(Arrays.toString(res));
+String res1[]=str.split("is");
+System.out.println(Arrays.toString(res1));
+}
+}
