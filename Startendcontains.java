06bff58fbac90fdc0c1bca252a44f8ee0078902e
kondurisathwika <kondurisathwika@gmail.com>
2024-07-09 19:50:45 +0530

    String methods

---
+++ Startendcontains.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Startendcontains
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str="Java is a Simple Language";
+System.out.println(str.startsWith("J"));
+System.out.println(str.endsWith("age"));
+System.out.println(str.contains("Simple"));
+System.out.println(str.startsWith("k"));
+}
+}
