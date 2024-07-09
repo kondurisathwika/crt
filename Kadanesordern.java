9dd26d0952bfe564de97817f96c8d96f8ef80d38
kondurisathwika <kondurisathwika@gmail.com>
2024-07-09 14:51:11 +0530

    Kadane's algorithm order of n

---
+++ Kadanesordern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Kadanesordern
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+int gs=a[0],cs=0;
+for(int i=0;i<n;i++)
+{
+cs=cs+a[i];
+if(gs<cs)
+gs=cs;
+if(cs<0)
+cs=0;
+}
+System.out.println(gs);
+}
+}
